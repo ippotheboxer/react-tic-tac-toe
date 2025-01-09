1a96ad3acd37cb5bcb281d66f71838f86349f5d6
Sophie <sophiesokol123@gmail.com>
2025-01-09 14:21:28 +0000

    Make log display latest moves

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
-const Log = () => {
-  return (<ol id='log'>
-
+const Log = ({ turns }) => {
+    return (<ol id='log'>
+        {turns.map(turn => 
+             <li key={`${turn.square.row}${turn.square.col}`}>
+                {turn.player} selected ({turn.square.row}, {turn.square.col})
+            </li>
+        )}
+       
     </ol>
   );
 }

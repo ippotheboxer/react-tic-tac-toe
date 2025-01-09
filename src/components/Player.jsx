a36e70ed0300ccb20224ec2ec50f973a2741ab51
Sophie <sophiesokol123@gmail.com>
2025-01-09 16:11:07 +0000

    Complete game

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,13 @@
 import React, {useState} from 'react';
 
-const Player = ({ initialName, symbol, isActive }) => {
+const Player = ({ initialName, symbol, isActive, onChangeName }) => {
     const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
     function handleEditClick() {
         setIsEditing(editing => !editing);
+        if (isEditing) {
+            onChangeName(symbol, playerName);
+        } 
     }
 
     let editablePlayerName = <span className="player-name">{playerName}</span>;
